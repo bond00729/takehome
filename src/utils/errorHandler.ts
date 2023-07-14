bca03f5824d634308acba10dad43d60a2c5ceef5
Matthew Bond <bond00729@users.noreply.github.com>
2023-07-14 16:46:58 -0700

    chore: update prisma error check
    Signed-off-by: Matthew Bond <bond00729@users.noreply.github.com>

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -2,14 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
 
 export function errorHandler(
-  err: Error,
+  err: Error & { code?: string },
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   if (err instanceof z.ZodError) {
     res.status(400).json(err.issues);
-  } else if (err.name === "NotFoundError") {
+  } else if (err.code === "P2025") {
     res.status(404).send("Not found");
   } else {
     res.status(500).send("Internal server error");
