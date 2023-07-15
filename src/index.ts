3e11a57c6ac353b19a091c0a55fe026fe9a611d7
Matthew Bond <bond00729@users.noreply.github.com>
2023-07-15 14:34:22 -0700

    feat(docs): init swagger
    Signed-off-by: Matthew Bond <bond00729@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 
 import { v1, v1Redirect } from './v1';
 import { errorHandler } from './utils/errorHandler';
 import { notFoundHandler } from './utils/notFoundHandler';
 import { asyncHandler } from './utils/asyncHandler';
 
+const swaggerSpec = swaggerJsDoc({
+  failOnErrors: true,
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Takehome',
+      version: '1.0.0',
+      description:
+        'This is a REST API made with Express for the takehome assessment.',
+      license: {
+        name: 'Licensed Under MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:8080',
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./src/v1/**/*.ts'],
+});
+
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1', v1);
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/:slug', asyncHandler(v1Redirect));
 
 app.use(notFoundHandler);
