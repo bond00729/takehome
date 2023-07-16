b958d9b92ed0b07a8ee3cd3dbe0fd32b3c5bf15b
Matthew Bond <bond00729@users.noreply.github.com>
2023-07-15 19:36:59 -0700

    chore: update error logging
    Signed-off-by: Matthew Bond <bond00729@users.noreply.github.com>

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -8,17 +8,18 @@ export function errorHandler(
   res: Response,
   _next: NextFunction
 ) {
-  logger.error(`server error: ${JSON.stringify(err, null, 2)}`);
-
   if (err instanceof z.ZodError) {
+    logger.warn(`server error: ${JSON.stringify(err, null, 2)}`);
     res
       .status(400)
       .json(
         err.issues.map(({ code, message, path }) => ({ code, message, path }))
       );
   } else if (err.code === 'P2025') {
+    logger.warn(`server error: ${JSON.stringify(err, null, 2)}`);
     res.status(404).send('Not found');
   } else {
+    logger.error(`unexpected server error: ${JSON.stringify(err, null, 2)}`);
     res.status(500).send('Internal server error');
   }
 }
