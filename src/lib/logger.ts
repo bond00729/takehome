e3efd8d7f8d8a093cd44d6dc2915972f830fe253
Matthew Bond <bond00729@users.noreply.github.com>
2023-07-16 10:01:13 -0700

    chore: add adjustable log levels via env var
    Signed-off-by: Matthew Bond <bond00729@users.noreply.github.com>

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,10 +1,13 @@
 import pino from 'pino';
 import pinoHttp from 'pino-http';
 
+const logLevel = process.env.LOG_LEVEL || 'info';
+
 export const httpLogger =
   process.env.NODE_ENV === 'production'
-    ? pinoHttp()
+    ? pinoHttp({ level: logLevel })
     : pinoHttp({
+        level: logLevel,
         transport: {
           target: 'pino-pretty',
         },
@@ -12,8 +15,9 @@ export const httpLogger =
 
 export const logger =
   process.env.NODE_ENV === 'production'
-    ? pino()
+    ? pino({ level: logLevel })
     : pino({
+        level: logLevel,
         transport: {
           target: 'pino-pretty',
         },
